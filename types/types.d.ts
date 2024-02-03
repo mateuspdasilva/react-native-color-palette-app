a0212c8f7770ec374be93dfef8ae47bc9b93abd5
mateuspdasilva <mpdasilvatreze@gmail.com>
2024-02-03 17:18:31 -0300

    Adds new types

--- types/types.d.ts
+++ types/types.d.ts
@@ -12,3 +12,48 @@ declare interface PalettePreviewProps {
   handlePress: () => void;
   colorPalette: ColorPalette;
 }
+
+interface ColorPaletteModalProps {
+  navigation: {
+    navigate: (screen: string, params: { newColorPalette: { paletteName: string; colors: Colors[] } }) => void;
+  };
+}
+
+interface ColorPaletteProps {
+  navigation: ColorPaletteModalProps['navigation'];
+  route: {
+    params: {
+        paletteName: string;
+        colors: Colors[];
+      };
+    };
+}
+
+declare interface HomeProps {
+  navigation: {
+    navigate: (screen: string, params?: { newColorPalette?: ColorPalette }) => void;
+  };
+  route: {
+    params?: {
+      newColorPalette?: ColorPalette;
+    };
+  };
+}
+
+type RootStackParamList = {
+  Main: undefined;
+  ColorPaletteModal: undefined;
+};
+
+type MainStackParamList = {
+  Home: undefined;
+  ColorPalette: { paletteName: string; colors: { colorName: string; hexCode: string }[] };
+};
+
+type AppNavigationProp = {
+  navigate: (screen: keyof RootStackParamList, params?: any) => void;
+};
+
+type MainScreenRouteProp = {
+  route: { params?: { paletteName?: string } };
+};
